584eaddee6c426ca8aa9cbac1e7fbc9a791a50e2
Andrew Seward <awswrd@gmail.com>
2015-07-09 18:01:02 -0700

    Sweet, sweet ES6

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,30 @@
 "use strict";
 
+require('shelljs/global');
+
 var gulp = require('gulp');
 var gulpList = require('gulp-task-listing');
 var electron = require('electron-prebuilt');
-require('shelljs/global');
+var es6transpiler = require('gulp-es6-transpiler');
+var path = require('path');
 
 gulp.task('help', gulpList);
 
 gulp.task('default', ['help']);
 
-gulp.task('run', function() {
-  var command = electron + " " + __dirname;
+gulp.task('build', function() {
+  gulp.src('package.json').
+    pipe(gulp.dest('dist'));
+
+  gulp.src('index.html').
+    pipe(gulp.dest('dist'));
+
+  gulp.src('main.js').
+    pipe(es6transpiler()).
+    pipe(gulp.dest('dist'));
+});
+
+gulp.task('run', ['build'], function() {
+  var command = electron + " " + path.join(__dirname, 'dist');
   exec(command, function(status, output) { });
 });
