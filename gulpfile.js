e0f20ba421ed6a57f08020ba89df0ac06f379a42
Andrew Seward <awswrd@gmail.com>
2015-07-12 13:52:33 -0700

    Fix eslint errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,33 @@
 "use strict";
 
-var del = require('del');
-var gulp = require('gulp');
-var gulpList = require('gulp-task-listing');
-var es6transpiler = require('gulp-es6-transpiler');
-var path = require('path');
-var shell = require('shelljs');
+var del = require("del");
+var gulp = require("gulp");
+var gulpList = require("gulp-task-listing");
+var es6transpiler = require("gulp-es6-transpiler");
+var path = require("path");
+var shell = require("shelljs");
 
-gulp.task('help', gulpList);
+gulp.task("help", gulpList);
 
-gulp.task('default', ['help']);
+gulp.task("default", ["help"]);
 
-gulp.task('clean', function() {
-  del(['./dist/*']);
+gulp.task("clean", function() {
+  del(["./dist/*"]);
 });
 
-gulp.task('build', ['clean'], function() {
-  gulp.src('package.json')
-    .pipe(gulp.dest('./dist'));
+gulp.task("build", ["clean"], function() {
+  gulp.src("package.json")
+    .pipe(gulp.dest("./dist"));
 
-  gulp.src('src/index.html')
-    .pipe(gulp.dest('./dist'));
+  gulp.src("src/index.html")
+    .pipe(gulp.dest("./dist"));
 
-  gulp.src('src/*.js')
+  gulp.src("src/*.js")
     .pipe(es6transpiler())
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest("./dist"));
 });
 
-gulp.task('run', ['build'], function() {
-  var command = require('electron-prebuilt') + " " + path.join(__dirname, 'dist');
-  shell.exec(command, function(status, output) { });
+gulp.task("run", ["build"], function() {
+  var command = require("electron-prebuilt") + " " + path.join(__dirname, "dist");
+  shell.exec(command);
 });
--- src/main.js
+++ src/main.js
@@ -1,26 +1,27 @@
-"use strict"
+"use strict";
 
-var app = require('app');
-var BrowserWindow = require('browser-window');
+var app = require("app");
+var BrowserWindow = require("browser-window");
+var path = require("path");
 
-require('crash-reporter').start();
+require("crash-reporter").start();
 
 var mainWindow = null;
 
-app.on('window-all-closed', () => {
-  if (process.platform != 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('ready', () => {
+app.on("ready", () => {
   mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.loadUrl(path.join("file://", __dirname, "index.html"));
 
   mainWindow.openDevTools();
 
-  mainWindow.on('closed', function() {
+  mainWindow.on("closed", function() {
     mainWindow = null;
   });
 });
